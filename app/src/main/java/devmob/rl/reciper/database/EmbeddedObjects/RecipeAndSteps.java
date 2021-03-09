d610894466ed043effa95b0ed2c30c0d904a31f0
ETIENNE LOIC <lo.etienne@student.helmo.be>
2021-03-09 23:12:21 +0100

    WIP : ajout de requêtes pour la DB + implémentation du one to many pour lier les étapes/ingrédients à une recette dans la DB

---
+++ app/src/main/java/devmob/rl/reciper/database/EmbeddedObjects/RecipeAndSteps.java
@@ -0,0 +1,33 @@
+package devmob.rl.reciper.database.EmbeddedObjects;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import devmob.rl.reciper.model.Recipe;
+import devmob.rl.reciper.model.Step;
+
+public class RecipeAndSteps {
+
+
+
+    @Embedded
+    public Recipe recipe;
+    @Relation(
+            parentColumn = "recipeId",
+            entityColumn = "recipeContainerId"
+    )
+    private List<Step> steps;
+
+    public List<Step> getSteps() {
+        System.out.println("steps called");
+        System.out.println(this.steps.size());
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+}
--- app/src/main/java/devmob/rl/reciper/database/dao/RecipeDao.java
+++ app/src/main/java/devmob/rl/reciper/database/dao/RecipeDao.java
@@ -5,12 +5,16 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 
 import java.util.List;
 import java.util.UUID;
 
+import devmob.rl.reciper.database.EmbeddedObjects.RecipeAndSteps;
+import devmob.rl.reciper.model.Ingredient;
 import devmob.rl.reciper.model.Recipe;
+import devmob.rl.reciper.model.Step;
 
 @Dao
 public interface RecipeDao {
@@ -21,22 +25,40 @@ public interface RecipeDao {
     @Query("SELECT * FROM recipe")
     LiveData<List<Recipe>> getRecipes();
 
-    @Query("SELECT * FROM recipe WHERE id = (:uuid)")
+    @Query("SELECT * FROM recipe WHERE recipeId = (:uuid)")
     LiveData<Recipe> getRecipe(final UUID uuid);
+
+    @Transaction
+    @Query("SELECT * FROM recipe WHERE recipeId = (:uuid)")
+    LiveData<RecipeAndSteps> getStepsByArtistId(final UUID uuid);
 
     // INSERT
 
     @Insert
-    void insert(final Recipe recipe);
+    void insertRecipe(final Recipe recipe);
+
+    @Insert
+    void insertStep(final Step step);
+
+    @Insert
+    void insertIngredient(final Ingredient ingredient);
 
     // UPDATE
 
     @Update
-    void update(final Recipe recipe);
+    void updateRecipe(final Recipe recipe);
 
     // DELETE
     @Delete
-    void delete(final Recipe recipe);
+    void deleteRecipe(final Recipe recipe);
+
+    @Delete
+    void deleteStep(final Step step);
+
+    @Delete
+    void deleteIngredient(final Ingredient ingredient);
+
+
 
 
 }
